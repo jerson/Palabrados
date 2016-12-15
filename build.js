0e2490c064390032fd741b5fdad05d0aea71dacf
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2016-12-15 12:21:41 -0500

    agregado imagenes

--- build.js
+++ build.js
@@ -2,8 +2,7 @@ var NwBuilder = require('nw-builder');
 var nw = new NwBuilder({
     files: './build/**/**', 
     platforms: ['osx64', 'win32', 'win64'],
-    macIcns:'./build/icon.icns',
-    winIco:'./build/favicon.ico',
+    buildDir:'./releases',
 });
 
 nw.on('log',  console.log);
---
+++ readme.md
@@ -0,0 +1,2 @@
+![Inicio](https://github.com/jerson/Palabrados/raw/master/doc/1.png)
+![Juego](https://github.com/jerson/Palabrados/raw/master/doc/2.png)
